162a4f9b43b07b9ac972b70ff8322d718968b26d
anima153 <anima.sharma15@gmail.com>
2020-07-14 13:06:18 +0530

    Update guidelines.page.ts

--- src/app/guidelines/guidelines.page.ts
+++ src/app/guidelines/guidelines.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoaderService } from '../services/loader.service';
 
 @Component({
   selector: 'app-guidelines',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuidelinesPage implements OnInit {
 
-  constructor() { }
+  constructor(private ionLoader: LoaderService) { }
 
-  ngOnInit() {
+ngOnInit() {
+    this.ionLoader.showHideAutoLoader();
   }
 
 }
