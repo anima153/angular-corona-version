dbedd75fbf590f1bd06ac395b24620963dd6d99e
anima153 <anima.sharma15@gmail.com>
2020-07-14 13:08:08 +0530

    Update news.page.ts

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../services/news.service';
+import { LoaderService } from '../services/loader.service';
+// import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPage implements OnInit {
 
-  constructor() { }
+
+  public iArticles: IArticles[];
+
+  constructor(private newsService: NewsService, private ionLoader: LoaderService) { }
 
   ngOnInit() {
+    this.ionLoader.showLoader();
+    this.newsService.getArticlesData().subscribe((data) => {
+      this.iArticles = data;
+      this.ionLoader.hideLoader();
+    });
   }
 
-}
+  openBrowser(url) {
+    // this.iab.create(url);
+    window.open(url, '_system');
+  }
+
+
+  trimSourceDetails(source: any): String {
+    return (source!=null) ? ((source.split(' ')[1]!=null) ? source.split(' ')[0] + ' ' + source.split(' ')[1] : source.split(' ')[0]) : source;
+    }
+    
+  }
+
