efe13c7da2e790666783661a389d9296810b74a0
anima153 <anima.sharma15@gmail.com>
2020-07-14 13:07:11 +0530

    Update help.page.ts

--- src/app/help/help.page.ts
+++ src/app/help/help.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { mailOutline, logoWebComponent, mailSharp, logoWhatsapp, callOutline, walletOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-help',
